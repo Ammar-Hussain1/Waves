c0913bdf1c1df5b926f7baeed2bd6de86c197a6f
zohapasha <zohapasha16@gmail.com>
2025-05-14 20:50:26 +0500

    the last commit

--- components/paki.jsx
+++ components/paki.jsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 function PakistanModel() {
-  const gltf = useLoader(GLTFLoader, '/pak.glb'); // Your Pakistan model
+  const gltf = useLoader(GLTFLoader, '/kara.glb'); 
   const ref = useRef();
 
   useFrame(() => {
